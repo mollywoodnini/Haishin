99d10fcc721aac4a9999b31c75978567c18f0661
Tan Nghia La <tan.la@eurowings.com>
2026-01-24 17:23:36 +0100

    Initial Commit

---
+++ Miru/MiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiruApp.swift
+//  Miru
+//
+//  Created by Tan Nghia La on 24.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiruTests/MiruTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiruTests.swift
+//  MiruTests
+//
+//  Created by Tan Nghia La on 24.01.26.
+//
+
+import Testing
+@testable import Miru
+
+struct MiruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
